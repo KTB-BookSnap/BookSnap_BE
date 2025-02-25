17551807a0da32be9a7249a919d8aac0737eb6d6
Changsu Park <gd12091@gmail.com>
2025-02-25 19:54:32 +0900

    Initial Commit

---
+++ src/main/java/server/booksnap/BooksnapApplication.java
@@ -0,0 +1,13 @@
+package server.booksnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksnapApplication.class, args);
+	}
+
+}
