b44c4f38d4b764ae222d7305ae8a5c82b38dab2e
Changsu Park <gd12091@gmail.com>
2025-02-26 15:12:06 +0900

    feat: Book Service & Controller

--- src/main/java/server/booksnap/service/BookService.java
+++ src/main/java/server/booksnap/service/BookService.java
@@ -1,14 +1,25 @@
 package server.booksnap.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import server.booksnap.domain.Book;
+import server.booksnap.dto.response.BookResponseDto;
+import server.booksnap.repository.BookRepository;
 
 @Service
 @RequiredArgsConstructor
 public class BookService {
 
-    public List<BookResponseDto> getBookList() {
+    private final BookRepository bookRepository;
 
+    public List<BookResponseDto> getBookList() {
+        return bookRepository.findAll().stream()
+                .map(book -> BookResponseDto.of(
+                        book.getId(),
+                        book.getTitle(),
+                        book.getThumbnailUrl()
+                )).collect(Collectors.toList());
     }
 }
