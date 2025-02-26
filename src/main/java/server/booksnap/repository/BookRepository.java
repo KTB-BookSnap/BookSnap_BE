3d023323d8954120b899e1052230f433a210aca6
koojun99 <koojun99@naver.com>
2025-02-26 16:52:57 +0900

    ✨ Feat: 북카드 생성 API

--- src/main/java/server/booksnap/repository/BookRepository.java
+++ src/main/java/server/booksnap/repository/BookRepository.java
@@ -1,7 +1,11 @@
 package server.booksnap.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.booksnap.domain.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    boolean existsByTitle(String title);
+
+    Optional<Book> findByTitle(String title);
 }
