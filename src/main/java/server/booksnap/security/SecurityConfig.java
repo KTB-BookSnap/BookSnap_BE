13ef386d7846118568091d1cd7d4100cd91256a7
Changsu Park <gd12091@gmail.com>
2025-02-27 15:24:49 +0900

    fix: CORS

--- src/main/java/server/booksnap/security/SecurityConfig.java
+++ src/main/java/server/booksnap/security/SecurityConfig.java
@@ -19,8 +19,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 적용
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 상태 관리
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").permitAll() // 특정 경로는 인증 없이 접근 가능
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("http://booksnap.site:*")); // 모든 도메인 허용
+        configuration.setAllowedOrigins(List.of("http://booksnap.site")); // 모든 도메인 허용
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // 크리덴셜 허용 (프론트에서 쿠키 필요시)
